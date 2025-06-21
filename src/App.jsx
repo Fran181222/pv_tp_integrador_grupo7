454a5e5c325b9423d4598d979a423115496a2e42
Francisco <uepeie13@gmail.com>
2025-06-21 18:40:35 -0300

    feature: detail

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
-import React, { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import Home from './views/Home';
-import NuevoProducto from './views/NuevoProducto';
-import { productosStore } from './data/productos';
-import './App.css'
+import Favorites from './views/Favorites';
+import ProductDetail from './views/ProductDetail';
+import FormView from './views/FormView';
 
-function App() {
-  const [productos, setProductos] = useState(productosStore);
-
-  const agregarProducto = (nuevo) => {
-    setProductos([...productos, nuevo]);
-  };
+export default function App() {
   return (
+    <div>
+      <nav className="p-4 bg-gray-200 flex gap-4">
+        <Link to="/">Home</Link>
+        <Link to="/favoritos">Favoritos</Link>
+        <Link to="/crear">Crear Producto</Link>
+      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/nuevo-producto" element={<NuevoProducto />} />
+        <Route path="/favoritos" element={<Favorites />} />
+        <Route path="/detalle/:id" element={<ProductDetail />} />
+        <Route path="/crear" element={<FormView />} />
+        <Route path="/editar/:id" element={<FormView editar />} />
       </Routes>
+    </div>
   );
 }
-
-export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
-import { BrowserRouter } from 'react-router-dom';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
 import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-)
+  </Provider>
+);
