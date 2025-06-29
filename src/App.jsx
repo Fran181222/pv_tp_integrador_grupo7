46e3f60cb69c5197aef80e56cd0fd9b835f42118
Ruancas <ruancas025@gmail.com>
2025-06-29 10:13:40 -0300

    una parte del css

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import Home from './views/Home';
 import Favorites from './views/Favorites';
 import ProductDetail from './views/ProductDetail';
 import FormView from './views/FormView';
+import './App.css';
 
 export default function App() {
   return (
     <div>
-      <nav className="p-4 bg-gray-200 flex gap-4">
+      <nav className="navbar">
         <Link to="/">Home</Link>
         <Link to="/favoritos">Favoritos</Link>
         <Link to="/crear">Crear Producto</Link>
